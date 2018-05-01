d06a51692d259786f5f2f53c06277586e571f481
Ryan Sumner <ryan.j.sumner@gmail.com>
2018-05-01 17:07:19 +1200

    Added Sound to Scene

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource HubStatistics;
+
+	// Use this for initialization
+	void Start () {
+		HubStatistics.loop = true;
+		HubStatistics.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
