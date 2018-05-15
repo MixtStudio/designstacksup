d1c4cc87ab48fcdee9fc1986b7115f13afed0571
Ryan Sumner <ryan.j.sumner@gmail.com>
2018-05-15 13:56:11 +1200

    commit

---
+++ Assets/Scripts/DesignDial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignDial : MonoBehaviour {
+
+	private Vector3 spawnPos;
+
+	public void SetSpawnPos(Vector3 pos) { spawnPos = pos; }
+	public Vector3 GetSpawnPos() { return spawnPos; }
+
+	public void Respawn() {
+		transform.localPosition = spawnPos;
+	}
+
+	void OnEnable() {
+		spawnPos = transform.position;
+	}
+	
+}
---
+++ Assets/Scripts/Shoe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoe : MonoBehaviour {
+
+	private Ascend ascend;
+
+	private void OnTriggerEnter(Collider col) {
+		Debug.Log("Collsion with: "+col);
+		if(col.gameObject.tag == "DesignDial") {
+			col.gameObject.GetComponent<DesignDial>().Respawn();
+			ascend.StartAscending();
+		}
+	}
+
+	// Use this for initialization
+	void OnEnable () {
+		ascend = FindObjectOfType<Ascend>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
