9d11b7315269054155661c5d97b7dcb5cc3881aa
Ryan Sumner <ryan.j.sumner@gmail.com>
2018-05-16 16:05:30 +1200

    Lazer Pointer
    Script attached to the "motion controllers" game object.
    finds the Control (one controller) child then starts using renderline to draw from the controller to the cursor.

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class LaserPointer : MonoBehaviour {
+
+	public GameObject target;
+	
+	private Transform controller;
+	private LineRenderer LR;
+	private bool draw;
+	private InteractionSourceState[] interactionSourceStates;
+	
+	
+	void Start () {		
+		LR = GetComponent<LineRenderer>();
+		
+	}
+	
+	void Update () {
+		if(controller == null){
+			LR.enabled = false;
+			Initialize();
+		}
+		
+		if(!draw && controller != null) {
+			interactionSourceStates = InteractionManager.GetCurrentReading();
+			foreach(InteractionSourceState interactionSourceState in interactionSourceStates){
+				if(interactionSourceState.anyPressed){
+					draw = true;
+					break;
+				}
+			}
+		}
+		
+		if(draw && controller != null)
+			DrawLaser();
+	}
+	
+	private void Initialize(){
+		controller = transform.GetChild(0);
+		if(controller != null)
+			LR.enabled = true;
+	}
+	
+	private void DrawLaser(){
+		Vector3[] positions = new Vector3[2];
+		positions[0] = controller.transform.position;
+		positions[1] = target.transform.position;
+		LR.positionCount = positions.Length;
+		LR.SetPositions(positions);
+	}
+}
